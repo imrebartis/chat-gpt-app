f781e7137017b4c5ff937a75eb1c920f636a6e68
Imre Bartis <imbartis@gmail.com>
2025-01-01 04:26:10 +0200

    add route & layout

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutRoute() {
+  return (
+    <main>
+      <h1 className='text-4xl font-bold'>About us</h1>
+      <div className='mt-5'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet a cupiditate quas quos veritatis ut tenetur dolore, numquam minus ratione nemo, non totam optio nobis nihil magni corrupti ducimus minima.
+      </div>
+    </main>
+  )
+}
