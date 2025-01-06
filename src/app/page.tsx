49404f4ef4996d9a4a0aa029172ef22ef6f80969
Imre Bartis <imbartis@gmail.com>
2025-01-06 03:05:51 +0200

    add authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
+import { auth as getServerSession } from '@/auth'
+import { Separator } from '@/components/ui/separator'
 import Chat from './components/Chat'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession()
+
   return (
     <main className='p-5'>
       <h1 className='text-4xl font-bold'>Welcome To GPT Chat</h1>
-      <Chat />
+      {!session?.user?.name && <div>You need to log in to use this chat.</div>}
+      {session?.user?.name && (
+        <>
+          <Separator className='my-5' />
+          <Chat />
+        </>
+      )}
     </main>
   )
 }
