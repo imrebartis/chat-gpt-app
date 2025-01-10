41a28b7c6aaea19c129f0941aed69e71d830b441
Imre Bartis <imbartis@gmail.com>
2025-01-10 14:40:07 +0200

    add access to previous chats

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from 'react'
 import { auth as getServerSession } from '@/auth'
 import { Separator } from '@/components/ui/separator'
 import Chat from './components/Chat'
+import PreviousChats from './components/PreviousChats'
 
 export default async function Home() {
   const session = await getServerSession()
@@ -11,6 +13,10 @@ export default async function Home() {
       {!session?.user?.email && <div>You need to log in to use this chat.</div>}
       {session?.user?.email && (
         <>
+          <Suspense fallback={<div>Loading Previous Chats</div>}>
+            <PreviousChats />
+          </Suspense>
+          <h4 className='mt-5 text-2xl font-bold'>New Chat Session</h4>
           <Separator className='my-5' />
           <Chat />
         </>
